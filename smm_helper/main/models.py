fcdcf5120af0238a51c2be208bad77fd795b285f
RezanansBLR <nikita19961032@gmail.com>
2023-04-14 16:24:52 +0300

    html

--- smm_helper/main/models.py
+++ smm_helper/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Bloger(models.Model):
+    title = models.TextField(max_length=100, verbose_name='Название аккаунта')
+    num_of_pubs = models.IntegerField(verbose_name='Количество публикаций')
+    num_of_subs = models.IntegerField(verbose_name='Количество подписчиков')
+    num_of_likers = models.IntegerField(verbose_name='Количество лайков')
+    prof_descr = models.TextField(max_length=250, verbose_name='Описание профиля')
+    contacts = models.TextField(max_length=100, verbose_name='Контакты')
+    last_pubs =  models.IntegerField(verbose_name='Количество публикаций за 30 дней')
+    last_likers = models.IntegerField(verbose_name='Количество лайков за 30 дней')
+    last_views = models.IntegerField(verbose_name='Количество просмотров за 30 дней')
+    last_coments = models.IntegerField(verbose_name='Количество комментариев за 30 дней')
+    er = models.IntegerField(verbose_name='Коеффициент вовлеченности')
+
+    class Meta:
+       verbose_name= 'Блогер'
+       verbose_name_plural = 'Блогеры'
