2cb0b8d28f5b3095b4b92f58a0ab7ef6ba16f0dd
RezanansBLR <nikita19961032@gmail.com>
2023-04-16 22:43:08 +0300

    urls, view, models and script

--- smm_helper/main/admin.py
+++ smm_helper/main/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Bloger
 
-admin.site.register(Bloger)
+class BlogerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug":("username",)}
+
+admin.site.register(Bloger, BlogerAdmin)
